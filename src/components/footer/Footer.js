32196ffb6723bed1d83618e381b156e42303ce02
Bartosz Kozecki <bartoszkozecki@192.168.1.8>
2023-02-06 20:09:49 +0100

    adding footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import context from "../utils/Context";
+
+import "./Footer.style.scss";
 
 const Footer = () => {
+  const ctx = useContext(context);
   return (
-    <footer>
+    <footer className={`${ctx.darkTheme ? "dark" : ""}`}>
       &copy; 2023 | Programmed by{" "}
       <a
         href="https://www.linkedin.com/in/bartosz-kozecki-ba798a197/"
         target="_blank"
+        className={`${ctx.darkTheme ? "dark" : ""}`}
       >
         bkozecki
       </a>{" "}
